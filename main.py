f31212313c202e8de2a3eb7a83c02a1025ec7ef0
gohdg <gohdg@naver.com>
2024-10-24 16:47:54 +0900

    CRUD(staus, HTTPException, Pydantic, typing)

--- main.py
+++ main.py
@@ -1,59 +1,143 @@
-from fastapi import FastAPI, Header
-from typing import Optional
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
 
-@app.get("/")
-async def read_root():
-    # return dict, fastapi가 json문자열로 변경해서 보내고 MIME 타입 또한 application/json으로 해서 응답
-    return {"message": "Hello World"}
+# Pyandatic class 정의: 유효성 체크를 위해서
 
 
-##################################################################################################
-# Path & Query Parameters
-# Path parameter: @app.get("/greet/{id}") 이렇게 정의되면 path parameter ({name})가 된다. handler에서 인자로 id를 받아야한다.
-# Query parameter: 라우트에 아무런 변수가 없는데, handler에 인자들이 존자하면 인자들은 query paramter가 된다.
-##################################################################################################
-
-@app.get("/greet")  # http://127.0.0.1:8000/greet?name=jonathan&age=28
-async def greet_name(name: Optional[str] = "User", age: int = 0) -> dict:
-    return {"message": f"Hello {name}", "age": age}
-
-
-# POST는 data가 전달되는데 일반적으로 data 유효성을 체크해야한다. 이때 사용되는것이 pydantic 모듈이다.
-# Pydantic 모듈의 사용법은 post에 전달되는 데이터 형태로 class를 정의하고 해당 클래스는 pydantic의 BaseModel을 상속 받아야한다.
-# 유효성의 체크는 handler에서 post data의 type을 정의한 pydantic class로 지정하면 된다.
-class BookCreateModel(BaseModel):
+class Book(BaseModel):
+    # create용 Pydantic class 정의: Create 시 유효성 체크를 위해서 사용
+    id: int
     title: str
     author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
 
 
-@app.post("/create_book")
-async def create_book(book_data: BookCreateModel):
-    return {
-        "title": book_data.title,
-        "author": book_data.author
-    }
+class BookUpdateModel(BaseModel):
+    # update에는 id를 업데이트 하면 안되니 update용 pydantic 클래스를 정의해서 유효성 체크
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
+
+# all books를 가져온다.
+# response 할 때도 pydantic을 이용해서 유효성 체크를 하도록 지정, 지정방법은 response_model 이떤 형태의 자료형인지 지정하면 된다.
 
 
-########################################################
-# Headers handling
-# fastapi 에서 Header 모듈 필요
-########################################################
+@app.get("/books", response_model=List[Book])
+async def get_all_books():
+    return books
 
-@app.get("/get_headers")
-async def get_headers(
-    accept: str = Header(None),
-    content_type: str = Header(None),
-    user_agent: str = Header(None),
-    host: str = Header(None)
-):
-    request_headers = {}
-    request_headers["Accept"] = accept
-    request_headers["Content-Type"] = content_type
-    request_headers["User-Agent"] = user_agent
-    request_headers["Host"] = host
 
-    return request_headers
+@app.post("/books", status_code=status.HTTP_201_CREATED)
+async def create_a_book(book_data: Book) -> dict:
+    # book_data 는 pydantic model인 Book 클래스의 인스턴스
+    # book_data.model_dump()는 인스턴스의 데이터를 dict로 변환한다.
+    new_book = book_data.model_dump()
+
+    books.append(new_book)
+
+    return new_book  # dict 인 new_book를 리턴하면 fastapi가 json 문자열로 변경
+
+
+@app.get("/book/{book_id}")
+async def get_book(book_id: int) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            return book
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Book not found"
+    )
+
+
+@app.patch("/book/{book_id}")
+async def update_book(book_id: int, book_update_data: BookUpdateModel) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = book_update_data.title
+            book["publisher"] = book_update_data.publisher
+            book["page_count"] = book_update_data.page_count
+            book["language"] = book_update_data.language
+
+            return book
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Book not found")
+
+
+@app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+# status 204는 body를 보내면 안된다. 따라서 handler의 return type ->dict를 하면 안된다.
+async def delete_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+
+            return {}
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Book not found")
